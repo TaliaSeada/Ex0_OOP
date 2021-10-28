b9cbef2c6deb261c9d9f1c5dda81d918ec9f7a32
Talia Seada <talias1235@gmail.com>
2021-10-28 15:49:39 +0300

    diagram photo

--- src/ex0/algo/AlgoTest.java
+++ src/ex0/algo/AlgoTest.java
@@ -21,23 +21,23 @@ class AlgoTest {
     Elevator elev9;
 
 
-    public AlgoTest(){
-        Simulator_A.initData(1,null);
+    public AlgoTest() {
+        Simulator_A.initData(1, null);
         b1 = Simulator_A.getBuilding();
         algo1 = new Algo(b1);
         elev1 = b1.getElevetor(0); //1
 
-        Simulator_A.initData(2,null);
+        Simulator_A.initData(2, null);
         b2 = Simulator_A.getBuilding();
         algo2 = new Algo(b2);
         elev2 = b1.getElevetor(0); //2
 
-        Simulator_A.initData(9,null);
+        Simulator_A.initData(9, null);
         b9 = Simulator_A.getBuilding();
         algo9 = new Algo(b9);
         elev9 = b9.getElevetor(0); //10
 
-        call = new CallForElevator() {
+        callUp = new CallForElevator() {
             @Override
             public int getState() {
                 return 0;
@@ -78,8 +78,15 @@ class AlgoTest {
 
         //At the start of the process, all the elevators are empty
         //and elevator 3 is the fastest so this is why this is the expected outcome
-        int elev = algo9.allocateAnElevator(call);
-        assertEquals(3,elev);
+        int elev1 = algo9.allocateAnElevator(callUp);
+        assertEquals(3, elev1);
+
+        int elev2 = algo9.allocateAnElevator(callDown);
+        assertEquals(1, elev2);
+
+
+
+
     }
 
     @Test
@@ -90,20 +97,25 @@ class AlgoTest {
           by the same way the source floor was added */
 
         MyQueue expected = new MyQueue(); //[0,-3,-4,-5,-1,0,19,40,70,19]
-        expected.add(0);  expected.add(-3);
-        expected.add(-4); expected.add(-5);
-        expected.add(-1); expected.add(0);
-        expected.add(19); expected.add(40);
-        expected.add(70); expected.add(19);
+        expected.add(0);
+        expected.add(-3);
+        expected.add(-4);
+        expected.add(-5);
+        expected.add(-1);
+        expected.add(0);
+        expected.add(19);
+        expected.add(40);
+        expected.add(70);
+        expected.add(19);
         /* In here we inserted the floors into the queue in the way they are supposed to
           be and then used our insert function to see if it inserts the floors correctly */
         int src1 = 0, src2 = -3, src3 = 70, src4 = 19, src5 = -1;
         int dest1 = -5, dest2 = -4, dest3 = 19, dest4 = 40, dest5 = 0;
-        algo9.insert(src1,dest1,0);
-        algo9.insert(src2,dest2,0);
-        algo9.insert(src3,dest3,0);
-        algo9.insert(src4,dest4,0);
-        algo9.insert(src5,dest5,0);
+        algo9.insert(src1, dest1, 0);
+        algo9.insert(src2, dest2, 0);
+        algo9.insert(src3, dest3, 0);
+        algo9.insert(src4, dest4, 0);
+        algo9.insert(src5, dest5, 0);
 
         //then compare between the results using the toString function we built
         assertEquals(expected.toString(), algo9.getCalls()[0].toString());
@@ -111,8 +123,9 @@ class AlgoTest {
         //simple check if call is inserted to the end of the queue
         int src6 = 100;
         int dest6 = 90;
-        expected.add(100);  expected.add(90);
-        algo9.insert(src6,dest6,0); //[0,-3,-4,-5,-1,0,19,40,70,19,100,90]
+        expected.add(100);
+        expected.add(90);
+        algo9.insert(src6, dest6, 0); //[0,-3,-4,-5,-1,0,19,40,70,19,100,90]
         assertEquals(expected.toString(), algo9.getCalls()[0].toString());
     }
 
@@ -123,20 +136,20 @@ class AlgoTest {
           elevator's queue */
         int src1 = 0, src2 = -1, src3 = 10, src4 = 6, src5 = -1;
         int dest1 = -2, dest2 = 4, dest3 = 3, dest4 = 9, dest5 = 0;
-        algo2.insert(src1,dest1,0);
-        algo2.insert(src2,dest2,0);
-        algo2.insert(src3,dest3,0);
+        algo2.insert(src1, dest1, 0);
+        algo2.insert(src2, dest2, 0);
+        algo2.insert(src3, dest3, 0);
         // =time = 22
         double time1 = algo2.timeToFloor(10, 0);
         assertEquals(time1, 22);
 
         int src6 = 0, src7 = -3, src8 = 70, src9 = 19, src10 = -1;
         int dest6 = -5, dest7 = -4, dest8 = 19, dest9 = 40, dest10 = 0;
-        algo9.insert(src6,dest6,0);
-        algo9.insert(src7,dest7,0);
-        algo9.insert(src8,dest8,0);
-        algo9.insert(src9,dest9,0);
-        algo9.insert(src10,dest10,0);
+        algo9.insert(src6, dest6, 0);
+        algo9.insert(src7, dest7, 0);
+        algo9.insert(src8, dest8, 0);
+        algo9.insert(src9, dest9, 0);
+        algo9.insert(src10, dest10, 0);
         double time2 = algo9.timeToFloor(96, 0);
         assertEquals(time2, 31.2);
     }
